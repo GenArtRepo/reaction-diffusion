921eabf6e3a6990ce2e6bbb188488b751d0a000a
Camilo Cruz Gambardella <camilo.cruzgambardella@monash.edu>
2022-08-03 11:37:21 +1000

    Changes to the intro comments

--- sketch.js
+++ sketch.js
@@ -1,20 +1,22 @@
 /*
 ** Reaction Diffusion
 * Cristian Rojas Cardenas, April 2022
-* Algorithm based on the tutorial of Daniel Shiffman.
-* See the video here: 
-* https://www.youtube.com/watch?v=BV9ny785UNc
+* Algorithm based on the tutorial by Karl Sims (https://www.karlsims.com/rd.html), and 
+* the video tutorial by Daniel Shiffman (https://www.youtube.com/watch?v=BV9ny785UNc).
 * 
-* The algorithm operates in a 2x2 grid, which stores values for two different chemicals. 
+* The algorithm operates on a 2D grid, which stores values for two different chemicals. 
 * The grid is initialized filled with one of the chemicals, and a square of 20x20 of the 
-* second chemical in the center. The interaction between the chemicals follows the next rules:
+* second chemical in the center. The interaction between the chemicals is driven by the following
+* equations:
 * 
 *   -	A’ = A + Da*(▽^2)*A – A*B^2 + f*(1 - A)
 *   -	B’ = B + Db*(▽^2)*B – (k + f)*B
 * 
-* The grid is computed and swapped in every frame. 
-* If you are interested in the theory behind the algorithm, you can visit 
-* http://karlsims.com/rd.html
+* Where Da and Db are the diffusion rates for each chemical, (▽^2)*A and (▽^2)*B are
+* 2D Laplacian functions that return the difference between the average content of each
+* chemical in all neighbouring cells and the content of the same chemical in the cell
+* being evaluated, and 'f' and 'k' are the feed and kill rates for the chemicals.
+* The grid is computed and updated at every time step. 
 * 
 */
 
@@ -23,6 +25,7 @@ let dA = 1;     //Diffusion rate A
 let dB = 0.5;   //Diffusion rate B
 var f = 0.055;  //Feed
 var k = 0.062;  //Kill
+
 
 
 // Settings, values used in the algorithm execution
@@ -52,6 +55,7 @@ function init(){
 
     grid = [];
     next = [];
+    
     // Initialize the grid
     for (let i = 0; i < width; i++) {
         grid[i] = [];
@@ -72,7 +76,7 @@ function init(){
 
 function setup(){
     gui();
-    createCanvas(720, 400); 
+    createCanvas(400, 400); 
     pixelDensity(1);
     background(255);
     init();
@@ -120,6 +124,7 @@ function draw(){
     }
 }
 
+// TODO #1 chemical is never used @crarojasca
 function laplaceA(x, y, chemical) {
   var sumA = 0;
   sumA += grid[x][y].a * -1;
