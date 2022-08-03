7b63dce6d3bdc38ad2aff9ecc5af406e3450d647
Camilo Cruz Gambardella <camilo.cruzgambardella@monash.edu>
2022-08-03 14:10:36 +1000

    Removed black borders

--- sketch.js
+++ sketch.js
@@ -21,27 +21,27 @@
 */
 
 let play = true;
-let dA = 1;     //Diffusion rate A
-let dB = 0.5;   //Diffusion rate B
+let dA = 1.75;   //Diffusion rate A
+let dB = 0.4;   //Diffusion rate B
 var f = 0.055;  //Feed
 var k = 0.062;  //Kill
 
 
 
 // Settings, values used in the algorithm execution
-let settings = { 
-  Play: function(){ play=true; },
-  Pause: function(){ play=false; },
-  Reset: function(){ init()},
+let settings = {
+    Play: function () { play = true; },
+    Pause: function () { play = false; },
+    Reset: function () { init() },
 };
 
-function gui(){
+function gui() {
     // Adding the GUI menu
     var gui = new dat.GUI();
     gui.width = 150;
-    gui.add(settings,'Play');
-    gui.add(settings,'Pause');
-    gui.add(settings,'Reset');
+    gui.add(settings, 'Play');
+    gui.add(settings, 'Pause');
+    gui.add(settings, 'Reset');
 };
 
 // At clicking the grid the state of the cell selected is changed
@@ -50,33 +50,33 @@ function mouseDragged() {
 }
 
 
-function init(){
-    central_point = [floor(width/2), floor(height/2)]
+function init() {
+    central_point = [floor(width / 2), floor(height / 2)]
 
     grid = [];
     next = [];
-    
+
     // Initialize the grid
     for (let i = 0; i < width; i++) {
         grid[i] = [];
         next[i] = []
         for (let j = 0; j < height; j++) {
             // Initial State starts with null quantities of a and b 
-            if(i > central_point[0]-10 & i < central_point[0]+10 
-                & j > central_point[1]-10 & j < central_point[1]+10){
-                grid[i][j] = { a:1, b:1 };
-            }else{
-                grid[i][j] = { a:1, b:0 };
+            if (i > central_point[0] - 10 & i < central_point[0] + 10
+                & j > central_point[1] - 10 & j < central_point[1] + 10) {
+                grid[i][j] = { a: 1, b: 1 };
+            } else {
+                grid[i][j] = { a: 1, b: 0 };
             }
-            next[i][j] = { a:1, b:1 };
-        } 
+            next[i][j] = { a: 1, b: 0 };
+        }
     }
 }
 
 
-function setup(){
+function setup() {
     gui();
-    createCanvas(400, 400); 
+    createCanvas(400, 400);
     pixelDensity(1);
     background(255);
     init();
@@ -85,19 +85,19 @@ function setup(){
 
 
 
-function draw(){
+function draw() {
 
-    if (play){
-        // Update the current values througth the diffusion formulas
+    if (play) {
+        // Update the current values using the diffusion formulas
         // Look at the theory in http://karlsims.com/rd.html
-        for (let i = 1; i < width-1; i++) {
-            for (let j = 1; j < height-1; j++) {
+        for (let i = 1; i < width - 1; i++) {
+            for (let j = 1; j < height - 1; j++) {
                 var a = grid[i][j].a
                 var b = grid[i][j].b
 
 
-                next[i][j].a = a + dA*laplaceA(i, j) - a*b*b + f*(1-a);
-                next[i][j].b = b + dB*laplaceB(i, j) + a*b*b - (k+f)*b;
+                next[i][j].a = a + dA * laplaceA(i, j) - a * b * b + f * (1 - a);
+                next[i][j].b = b + dB * laplaceB(i, j) + a * b * b - (k + f) * b;
 
 
                 next[i][j].a = constrain(next[i][j].a, 0, 1);
@@ -109,7 +109,7 @@ function draw(){
         loadPixels();
         for (let i = 0; i < width; i++) {
             for (let j = 0; j < height; j++) {
-                var pix = (i + j * width)*4;
+                var pix = (i + j * width) * 4;
                 c = floor((next[i][j].a - next[i][j].b) * 255);
                 c = constrain(c, 0, 255);
                 pixels[pix + 0] = c;
@@ -122,33 +122,35 @@ function draw(){
 
         grid = next;
     }
+
+    text(floor(frameRate()), 10, 10);
 }
 
 // TODO #1 chemical is never used @crarojasca
 function laplaceA(x, y, chemical) {
-  var sumA = 0;
-  sumA += grid[x][y].a * -1;
-  sumA += grid[x - 1][y].a * 0.2;
-  sumA += grid[x + 1][y].a * 0.2;
-  sumA += grid[x][y + 1].a * 0.2;
-  sumA += grid[x][y - 1].a * 0.2;
-  sumA += grid[x - 1][y - 1].a * 0.05;
-  sumA += grid[x + 1][y - 1].a * 0.05;
-  sumA += grid[x + 1][y + 1].a * 0.05;
-  sumA += grid[x - 1][y + 1].a * 0.05;
-  return sumA;
+    var sumA = 0;
+    sumA += grid[x][y].a * -1;
+    sumA += grid[x - 1][y].a * 0.2;
+    sumA += grid[x + 1][y].a * 0.2;
+    sumA += grid[x][y + 1].a * 0.2;
+    sumA += grid[x][y - 1].a * 0.2;
+    sumA += grid[x - 1][y - 1].a * 0.05;
+    sumA += grid[x + 1][y - 1].a * 0.05;
+    sumA += grid[x + 1][y + 1].a * 0.05;
+    sumA += grid[x - 1][y + 1].a * 0.05;
+    return sumA;
 }
 
 function laplaceB(x, y) {
-  var sumB = 0;
-  sumB += grid[x][y].b * -1;
-  sumB += grid[x - 1][y].b * 0.2;
-  sumB += grid[x + 1][y].b * 0.2;
-  sumB += grid[x][y + 1].b * 0.2;
-  sumB += grid[x][y - 1].b * 0.2;
-  sumB += grid[x - 1][y - 1].b * 0.05;
-  sumB += grid[x + 1][y - 1].b * 0.05;
-  sumB += grid[x + 1][y + 1].b * 0.05;
-  sumB += grid[x - 1][y + 1].b * 0.05;
-  return sumB;
+    var sumB = 0;
+    sumB += grid[x][y].b * -1;
+    sumB += grid[x - 1][y].b * 0.2;
+    sumB += grid[x + 1][y].b * 0.2;
+    sumB += grid[x][y + 1].b * 0.2;
+    sumB += grid[x][y - 1].b * 0.2;
+    sumB += grid[x - 1][y - 1].b * 0.05;
+    sumB += grid[x + 1][y - 1].b * 0.05;
+    sumB += grid[x + 1][y + 1].b * 0.05;
+    sumB += grid[x - 1][y + 1].b * 0.05;
+    return sumB;
 }
